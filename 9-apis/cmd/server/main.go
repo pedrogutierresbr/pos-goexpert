ed1770737d3345fe8ebbd9c6297a7f76ccff610f
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-07-12 10:44:52 -0300

    ajustando package para os handlers

--- 9-apis/cmd/server/main.go
+++ 9-apis/cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/pedrogutierresbr/pos-goexpert/9-apis/configs"
-	"github.com/pedrogutierresbr/pos-goexpert/9-apis/internal/dto"
 	"github.com/pedrogutierresbr/pos-goexpert/9-apis/internal/entity"
 	"github.com/pedrogutierresbr/pos-goexpert/9-apis/internal/infa/database"
+	"github.com/pedrogutierresbr/pos-goexpert/9-apis/internal/infa/webserver/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -24,38 +23,8 @@ func main() {
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 
 	productDB := database.NewProduct(db)
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 
 	http.HandleFunc("/products", productHandler.CreateProductInput)
 	http.ListenAndServe(":8000", nil)
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProductInput(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product) //decodifica e salva no product
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	p, err := entity.NewProduct(product.Name, product.Price) // cria um novo produto e salva no p
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	err = h.ProductDB.Create(p) // salva o produto criado no db
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
 }
---
+++ 9-apis/internal/infa/webserver/handlers/product_handlers.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/pedrogutierresbr/pos-goexpert/9-apis/internal/dto"
+	"github.com/pedrogutierresbr/pos-goexpert/9-apis/internal/entity"
+	"github.com/pedrogutierresbr/pos-goexpert/9-apis/internal/infa/database"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProductInput(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product) //decodifica e salva no product
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price) // cria um novo produto e salva no p
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p) // salva o produto criado no db
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
