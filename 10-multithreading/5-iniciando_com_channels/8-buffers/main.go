03ff26eebf65e2d657bc8dc8fe396498c6c2980c
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-08-04 16:41:57 -0300

    adicionado comentario a aula

--- 10-multithreading/5-iniciando_com_channels/8-buffers/main.go
+++ 10-multithreading/5-iniciando_com_channels/8-buffers/main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
+	// quando criar um chanel, e colocar um numero do lado, quer dizer que esta definindo buffer
 	ch := make(chan string, 2)
 	ch <- "Hello"
 	ch <- "World"
