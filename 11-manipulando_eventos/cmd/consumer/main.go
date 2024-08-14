9732efef516e8422d505c8ddceb87eb3a4b32841
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-08-13 22:38:20 -0300

    criado um consumidor na pratica no cmd

---
+++ 11-manipulando_eventos/cmd/consumer/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pedrogutierresbr/pos-goexpert/11-manipulando_eventos/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, msgs)
+
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
+
+// sempre levantar o container do rabbitmq antes
+// criar uma fila no rabbitmq adm
+
+// depois rodar a aplicação
