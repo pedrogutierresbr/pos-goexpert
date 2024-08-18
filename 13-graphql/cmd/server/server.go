7e69314ac1db35de7b30fc96dfc740d1732dedd5
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-08-18 00:34:47 -0300

    criando conexao com o banco de dados e persistindo uma categoria via playground

--- 13-graphql/cmd/server/server.go
+++ 13-graphql/cmd/server/server.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/pedrogutierresbr/pos-goexpert/12-graphql/graph"
+	"github.com/pedrogutierresbr/pos-goexpert/12-graphql/internal/database"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	// criado conexão com o banco de dados
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	// criado arquivo categoria que manipula o banco de dados
+	categoryDb := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	// injeto essa categoria no resolver
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
--- 13-graphql/internal/database/category.go
+++ 13-graphql/internal/database/category.go
@@ -13,9 +13,13 @@ type Category struct {
 	Description string
 }
 
+func NewCategory(db *sql.DB) *Category {
+	return &Category{db: db}
+}
+
 func (c *Category) Create(name string, description string) (Category, error) {
 	id := uuid.New().String()
-	_, err := c.db.Exec("INSERT INTE categories (id, name, description) VALUES ($1, $2, $3)", id, name, description)
+	_, err := c.db.Exec("INSERT INTO categories (id, name, description) VALUES ($1, $2, $3)", id, name, description)
 	if err != nil {
 		return Category{}, err
 	}
