a2a443e478c81df6f55a1bf577c11031c3d4c5cf
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-09-08 18:44:08 -0300

    implementado NewUow, Register e UnRegister

--- 18-UOW/pkg/uow/uow.go
+++ 18-UOW/pkg/uow/uow.go
@@ -10,8 +10,29 @@ type RepositoryFactory func(tx *sql.Tx) interface{}
 type UowInterface interface {
 	Register(name string, fc RepositoryFactory)
 	GetRepository(ctx context.Context, name string) (interface{}, error)
-	Do(ctx context.Context, fn func(uow *UowInterface) error) error
+	Do(ctx context.Context, fn func(uow *Uow) error) error
 	CommitOrRollback() error
 	Rollback() error
 	UnRegister(name string)
+}
+
+type Uow struct {
+	Db           *sql.DB
+	Tx           *sql.Tx
+	Repositories map[string]RepositoryFactory
+}
+
+func NewUow(ctx context.Context, db *sql.DB) *Uow {
+	return &Uow{
+		Db:           db,
+		Repositories: make(map[string]RepositoryFactory),
+	}
+}
+
+func (u *Uow) Register(name string, fc RepositoryFactory) {
+	u.Repositories[name] = fc
+}
+
+func (u *Uow) UnRegister(name string) {
+	delete(u.Repositories, name)
 }
