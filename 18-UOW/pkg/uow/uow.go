e20d788486c16e05cb9752ef54e7fa2cf7b95978
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-09-10 00:05:45 -0300

    implementado Do, Rollback e CommitOrRollback

--- 18-UOW/pkg/uow/uow.go
+++ 18-UOW/pkg/uow/uow.go
@@ -3,6 +3,8 @@ package uow
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 )
 
 type RepositoryFactory func(tx *sql.Tx) interface{}
@@ -35,4 +37,52 @@ func (u *Uow) Register(name string, fc RepositoryFactory) {
 
 func (u *Uow) UnRegister(name string) {
 	delete(u.Repositories, name)
+}
+
+func (u *Uow) Do(ctx context.Context, fn func(Uow *Uow) error) error {
+	// inicializado transação
+	if u.Tx != nil {
+		return fmt.Errorf("Transaction already started")
+	}
+	tx, err := u.Db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil
+	}
+	u.Tx = tx
+
+	// executar a fn com a transação
+	err = fn(u)
+	if err != nil {
+		errRb := u.Rollback()
+		if errRb != nil {
+			return errors.New(fmt.Sprintf("Original error: %s, rollback error: %s", err.Error(), errRb.Error()))
+		}
+		return err
+	}
+	return u.CommitOrRollback()
+}
+
+func (u *Uow) Rollback() error {
+	if u.Tx == nil {
+		return errors.New("No transaction to rollback")
+	}
+	err := u.Tx.Rollback()
+	if err != nil {
+		return err
+	}
+	u.Tx = nil
+	return nil
+}
+
+func (u *Uow) CommitOrRollback() error {
+	err := u.Tx.Commit()
+	if err != nil {
+		errRb := u.Rollback()
+		if errRb != nil {
+			return errors.New(fmt.Sprintf("Original error: %s, rollback error: %s", err.Error(), errRb.Error()))
+		}
+		return err
+	}
+	u.Tx = nil
+	return nil
 }
