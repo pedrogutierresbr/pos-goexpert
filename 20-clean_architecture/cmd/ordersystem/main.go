bf777b8c59fe004270b2e6ef1f50791ef9f09823
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-09-23 08:48:58 -0300

    reorganizado pastas e configurado vars de ambiente

---
+++ 20-clean_architecture/cmd/ordersystem/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/pedrogutierresbr/pos-goexpert/20-clean_arch/configs"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
